dc42296372dbcc2fa642ca339aa5903bcb8167a0
brett <brett@bmb.io>
2021-01-26 11:51:08 +0000

    add a more in depth test

--- reader_test.go
+++ reader_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"io/ioutil"
 	"testing"
 )
 
@@ -19,4 +20,28 @@ func TestReadBasic(t *testing.T) {
 		t.Errorf("Expected Arg0 = find me, got %s", res["Arg0"])
 	}
 
+}
+
+func TestReadFull(t *testing.T) {
+	// A stdin replica
+	var stdin bytes.Buffer
+	data, err := ioutil.ReadFile("./tst-in")
+	stdin.Write(data)
+
+	res, err := Read(&stdin)
+	if err != nil {
+		t.Errorf("Expected no error, got %v", err)
+	}
+
+	if res["argCount"] != "2" {
+		t.Errorf("Expected argCount = 2, got %s", res["argCount"])
+	}
+
+	if res["Arg0"] != "read" {
+		t.Errorf("Expected Arg0 = read, got %s", res["Arg0"])
+	}
+
+	if res["Arg1"] != "//path/to/file" {
+		t.Errorf("Expected Arg1 = //path/to/file, got %s", res["Arg1"])
+	}
 }
